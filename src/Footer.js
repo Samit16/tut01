5aa1bb6b6f5a6b5a8f82ae123be32b04068a0886
Samit Bhisikar <samitbhisikar16@gmail.com>
2026-02-22 22:12:15 +0530

    Props and drilling

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,8 @@
-const Footer = () => {
+const Footer = ({length}) => {
     const today=new Date();
     return (
-        <footer style={{
-            backgroundColor: 'lightblue',
-            color: '# 333',
-            padding: '10px',
-            position: 'fixed',
-            bottom: 0,
-            width: '100%',
-        }}>
-            <p>Copyright &copy; {today.getFullYear()}</p>
+        <footer>
+            <p>{length} List {length===1 ? "item":"items"}</p>
         </footer>
     )
 }
