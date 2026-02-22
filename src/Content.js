ee9032f26e34abe44c3e0c93451f1ac499a11ad3
Samit Bhisikar <samitbhisikar16@gmail.com>
2026-02-22 21:42:03 +0530

    changed looks

--- src/Content.js
+++ src/Content.js
@@ -5,7 +5,7 @@ const Content = () => {
     const [items, setItems] = useState([
         {
             id: 1,
-            checked:false,
+            checked:true,
             item: "One half pound bag of Cocoa Covered Almonds Unsweetened"
         },
         {
@@ -15,11 +15,16 @@ const Content = () => {
         },
         {
             id: 3,
-            checked:false,
+            checked:true,
             item: "Item 3"
         }
     ]);
 
+    const handleCheck = (id) => {
+        const listItems=items.map((item)=>item.id===id ? {...item, checked:!item.checked} : item);
+        setItems(listItems);
+        localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+    }
     return (
         <main>
             <ul>
@@ -28,9 +33,12 @@ const Content = () => {
                         <li className="item" key={item.id}>
                             <input 
                                 type="checkbox"
+                                onChange={() => handleCheck(item.id)}
                                 checked={ item.checked }
                             />
-                            <label>{ item.item }</label>
+                            <label onDoubleClick={() => handleCheck(item.id)}> 
+                                { item.item }
+                            </label>
                             <FaTrashAlt 
                                 role="button" 
                                 tabIndex="0" 
